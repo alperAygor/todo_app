fc5db7442eafd653d9a0e5617a9fd03c54bd973a
Alper Aygördüler <alperaygorduler61@gmail.com>
2023-06-23 15:36:33 +0300

    UI.js

--- todo_app/UI.js
+++ todo_app/UI.js
@@ -1,115 +1,120 @@
-import { Task } from "./task.js";
-export class UI{
-    constructor(){this.url="https://6492e665428c3d2035d0ddb2.mockapi.io/tasks"}
-    
-    static getTasks() {
-      return fetch(this.url, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      })
-        .then((response) => {
-          if (response.ok) {
-            UI.showAlert("success", "Başarılı bir şekilde eklendi");
-            return response.json();
-          } else {
-            throw new Error("Network response was not ok.");
-          }
-        })
-        .catch((error) => {
-          UI.showAlert("danger", "Beklenmeyen bir hata oluştu");
-          console.error(error);
+export class UI {
+  static url = "https://6492e665428c3d2035d0ddb2.mockapi.io/tasks";
+  static taskList = [];
+
+  static showAlert(alertType, message) {
+    const header = document.querySelector(".header");
+    const alert = document.createElement("div");
+    alert.classList=`container alert-${alertType}`;
+    alert.textContent = message;
+    header.appendChild(alert);
+
+    setTimeout(() => {
+      alert.remove();
+    }, 2750);
+  }
+
+  static  displayTask() {
+      const ul = document.querySelector(".list-group");
+      ul.innerHTML = "";
+      console.log(Array.isArray(this.taskList)); // true
+
+      this.taskList.forEach((task) => {
+      const li = document.createElement("li");
+      li.classList.add("list-group-item", "d-flex", "py-3");
+      li.setAttribute("data-id", task.id);
+
+      const h4 = document.createElement("h4");
+      h4.classList.add("h4", "task-title", "mx-2", "my-auto");
+      h4.textContent = task.taskTitle;
+
+      const p = document.createElement("p");
+      p.classList.add("p", "task-content", "mx-5", "my-auto");
+      p.textContent = task.taskContent;
+
+      const div = document.createElement("div");
+      div.classList.add("my-auto", "ms-auto");
+
+      const editIcon = document.createElement("i");
+      editIcon.classList.add("fa-solid", "fa-pen-to-square", "btn", "btn-warning", "mx-2", "ms-auto");
+      editIcon.id = "edit";
+
+      const deleteIcon = document.createElement("i");
+      deleteIcon.classList.add("fa-solid", "fa-trash", "btn", "btn-danger", "mx-2", "ms-auto");
+      deleteIcon.id = "delete";
+
+      div.appendChild(editIcon);
+      div.appendChild(deleteIcon);
+
+      li.appendChild(h4);
+      li.appendChild(p);
+      li.appendChild(div);
+
+      ul.appendChild(li);
+    });
+  }
+
+  static async saveInput(type, id) {
+    const addBtn = document.querySelector("#addBtn");
+    const titleInput = document.querySelector("#titleInput").value;
+    const contentInput = document.querySelector("#contentInput").value;
+
+    if (type === "edit") {
+      const updatedTask = {
+        taskTitle: titleInput,
+        taskContent: contentInput
+      };
+
+      try {
+        // const response = 
+        await fetch(`${UI.url}/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(updatedTask)
         });
-    }
-    
-    static  addTask(newTask){
-        fetch(this.url,{
+        // if(!response.ok){throw new error("hata")}
+        // this.taskList = await response.json();
+        //  UI.displayTask();
+        UI.showAlert("success", "Başarılı bir şekilde güncellendi");
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      const newTask = {
+        taskTitle: titleInput,
+        taskContent: contentInput
+      };
+
+      try {
+        // const response = 
+        await fetch(UI.url, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(newTask)
-        })
-        .then(response => response.json())
-        .then(data => {
-          UI.showAlert("success","Başarılı bir şekilde eklendi")
-        })
-        .catch(() => {
-          UI.showAlert("danger","Beklenmeyen bir hata oluştu")
         });
-        
-    }
-    static  deleteTask(id){
-      fetch(this.url+"/"+id,{
-        method: 'DELETE'
-      
-      })
-      .then(response => response.json())
-      .then(data => {
-        UI.showAlert("success","Başarılı bir şekilde silindi")
-      })
-      .catch(error => {
-        UI.showAlert("danger","Beklenmeyen bir hata oluştu")
-      });
-    }
-    static  editTask(updatedTask,id){
-      fetch(this.url+"/"+id,{
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedTask)
-      })
-      .then(response => response.json())
-      .then(data => {
-        UI.showAlert("success","Başarılı bir şekilde güncellendi")
-      })
-      .catch(() => {
-        UI.showAlert("danger","Beklenmeyen bir hata oluştu")
-      });
-    }
-    static showAlert(alertType,message){
-        const header=document.querySelector(".header");
-        const alert=`<div class="container alert alert-${alertType}">${message}</div>`
-        header.insertAdjacentHTML("beforeend",alert);
-        setTimeout(()=>{
-          const alert=document.querySelector(".alert");  
-          alert.remove();
-        },2750);
-    }
-    static displayTask(data){
-        console.log(typeof(data));
-        const ul = document.querySelector(".list-group");
-        ul.innerHTML = "";
-    
-        data.forEach(task => {
-          let li = `<li class="list-group-item d-flex py-3" data-id="${task.id}">
-                      <input type="checkbox" class="form-check-input my-auto me-2" >    
-                      <h4 class="h4 task-title mx-2 my-auto">${task.title}</h4>
-                      <p class="p task-content mx-5 my-auto">
-                        ${task.content}
-                      </p>  
-                      <div class="ms-auto my-auto">
-                        <i class="fa-solid fa-pen-to-square btn btn-warning mx-2" id="edit"></i>
-                        <i class="fa-solid fa-trash btn btn-danger mx-2" id="delete"></i>
-                      </div>  
-                    </li>`;
-          ul.insertAdjacentHTML("beforeend",li);
-        });
-      
-
-      } 
-      static saveInput(){
-        const titleInput=document.querySelector("#titleInput").value;
-        const contentInput=document.querySelector("#contentInput").value;
-        if(titleInput==="" || contentInput===""){
-            UI.showAlert("warning","Lütfen Alanları doğru şekilde doldurunuz!")
-    
-        }
-        else{
-            const task=new Task(titleInput,contentInput,false);
-             UI.addTask(task.taskObject);
-              ul.innerHTML=``;
-              addBtn.disabled=false;
-        }
+        // if(!response.ok){throw new error("hata")}
+      //   this.taskList = await response.json();
+      //  UI.displayTask();
+        UI.showAlert("success", "Başarılı bir şekilde eklendi");
+      } catch (error) {
+        console.error(error);
       }
     }
+
+    addBtn.disabled = false;
+  }
+
+  static async init() {
+    try {
+      const response = await fetch(UI.url);
+      this.taskList = await response.json();
+     UI.displayTask();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
